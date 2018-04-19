39c647095d0edfe38220379af2334815e0f7734d
rtacuna <rtacuna@uc.cl>
2018-04-19 19:25:04 -0300

    Definicion de clases

---
+++ objects.py
@@ -0,0 +1,117 @@
+
+author = 'rtacuna'
+
+
+class Camion:
+
+    def __init__(self, ruta, carga):
+        self.carga = carga
+        self.ruta = ruta
+        self.tiempo_llegada = 0
+
+
+class Camino:
+
+    def __init__(self, name, distancia, tiempo_viaje, origen, destino):
+        self.name = name
+        self.lista_camiones = []
+        self.tiempo_mas_cercano = -1
+        self.distancia = distancia
+        self.tiempo_viaje = tiempo_viaje
+        self.origen = origen
+        self.destino = destino
+
+        # Estadisticas
+
+        self.numero_camiones_pasados = 0
+
+    def llega_camion(self, camion):
+        self.lista_camiones.append(camion)
+
+    def sale_camion(self):
+        camion = self.lista_camiones.pop(0)
+        return camion
+
+    def actualizar_tiempo(self):
+        pass
+
+
+class Sink:
+
+    def __init__(self, name):
+        self.name = name
+        self.lista_camiones = []
+
+        # Estadisticas
+
+        self.cantidad_cobre = 0
+        self.cantidad_camiones = 0
+
+    def llega_camion(self, camion):
+        self.lista_camiones.append(camion)
+        self.cantidad_cobre = camion.carga
+        self.cantidad_camiones += 1
+
+
+class Bodega:
+
+    def __init__(self, name, capacidad):
+        self.name = name
+        self.bodega = 0
+        self.tiempo_salida = -1
+        self.capacidad = capacidad
+
+        # Estadisticas
+
+        self.camiones_salieron = 0
+        self.camiones_llegaron = 0
+
+    def llega_camion(self, camion):
+        self.cambia_cobre(camion.carga)
+        self.camiones_llegaron += 1
+
+    def sale_camion(self, camion):
+        self.cambia_cobre(-camion.carga)
+        self.camiones_salieron += 1
+
+    def cambia_cobre(self, cantidad):
+        self.bodega += cantidad
+
+    def actualiza_tiempo(self):
+        pass
+
+
+class Source:
+
+    def __init__(self, name):
+        self.name = name
+        self.tiempo_salida = -1
+
+        # Estadistica
+
+        self.camiones_salieron = 0
+
+    def sale_camion(self):
+        self.camiones_salieron +=1
+
+    def actualiza_tiempo(self):
+        pass
+
+
+class Puerto:
+
+    def __init__(self):
+        pass
+
+
+class Tren:
+
+    def __init__(self):
+        pass
+
+
+class CaminoTren:
+
+    def __init__(self):
+        pass
+
